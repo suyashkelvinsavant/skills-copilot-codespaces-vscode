0479d0be7690c31a81b92560e43bcfcd1236e540
Suyash Kelvin Savant <61877180+suyashkelvinsavant@users.noreply.github.com>
2023-12-20 10:55:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    if (document.getElementById('skills').value == 'member') {
+        document.getElementById('skillsMember').style.display = 'block';
+    }
+    else document.getElementById('skillsMember').style.display = 'none';
+}
